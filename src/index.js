bc54af680a60013ba5ad101993e69244a95de619
PotatoSol <sol.mike.j@gmail.com>
2023-02-10 09:17:31 -0800

    added a bit more ui, began logic for taking inputs, initialized currency object class

--- src/index.js
+++ src/index.js
@@ -7,3 +7,18 @@
 //  * for invalid currency
 //  * for non-200 api response
 
+handleSubmission(){
+  let userInput = new Currency(
+    document.getElementById("amountInput").value,
+    document.getElementById("nativeInput").value,
+    document.getElementById("targetInput").value
+  );
+}
+
+
+window.addEventListener("load", function(){
+  document.getElementById("userInput").addEventListener("submit", function(event){
+    event.preventDefault();
+    handleSubmission();
+  });
+});
