a9b9f2741bdd211dfffd5caf1d7e506feb442f08
PotatoSol <sol.mike.j@gmail.com>
2023-02-10 13:49:37 -0800

    remove unnecessary commented code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-//get user amount
-//get user currency
-//get target currency
-//return target currency
-
-//error check - on page
-//  * for invalid currency
-//  * for non-200 api response
 import {Currency} from "./js/currency.js";
 
 function handleSubmission(){  
@@ -14,14 +6,13 @@ function handleSubmission(){
     document.getElementById("nativeInput").value,
     document.getElementById("targetInput").value
   ).then(function(response){
-    //console.log(response);
     document.getElementById("output").innerText = (Intl.NumberFormat(document.getElementById("targetInput").value, {style: 'currency', currency: document.getElementById("targetInput").value}).format(response[0].conversion_result));
   }).catch(function(error){
     let errorResult = error;
     if(errorResult[0].status == 404){
       document.getElementById("output").innerText = "404: One of the currencies not found, check your inputs!";
     } else {
-      document.getElementById("output").innerText = errorResult[0].status + ": " + errorResult[0].statusText;
+      console.log(error);
     }
   });
 }
