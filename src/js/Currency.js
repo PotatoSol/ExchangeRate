c00dabc501d8449d3ac48186a66edddec3b1290c
PotatoSol <sol.mike.j@gmail.com>
2023-02-10 09:22:32 -0800

    added necessary parameters and functions to Currency object

--- src/js/Currency.js
+++ src/js/Currency.js
@@ -1,1 +1,17 @@
 //currency object
+export class Currency{
+  constructor(amount, native, target){
+    this.amount = amount;
+    this.native = native;
+    this.target = target;
+  }
+
+  calculateConversion(){
+
+  }
+
+  checkValidCurrency(){
+
+  }
+}
+
