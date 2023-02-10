a38b9766f35677e261b63a88abb7c1e3655f6623
PotatoSol <sol.mike.j@gmail.com>
2023-02-10 12:21:29 -0800

    some error catching and ui fancifying

--- src/index.js
+++ src/index.js
@@ -8,14 +8,21 @@
 //  * for non-200 api response
 import {Currency} from "./js/currency.js";
 
-function handleSubmission(){
+function handleSubmission(){  
   Currency.calculateConversion(
     document.getElementById("amountInput").value,
     document.getElementById("nativeInput").value,
     document.getElementById("targetInput").value
   ).then(function(response){
-    //console.log(response[0].conversion_result);
-    document.getElementById("output").innerText = (Intl.NumberFormat().format(response[0].conversion_result) + " " + document.getElementById("targetInput").value.  toUpperCase());
+    //console.log(response);
+    document.getElementById("output").innerText = (Intl.NumberFormat(document.getElementById("targetInput").value, {style: 'currency', currency: document.getElementById("targetInput").value}).format(response[0].conversion_result));
+  }).catch(function(error){
+    let errorResult = error;
+    if(errorResult[0].status == 404){
+      document.getElementById("output").innerText = "404: One of the currencies not found, check your inputs!";
+    } else {
+      document.getElementById("output").innerText = errorResult[0].status + ": " + errorResult[0].statusText;
+    }
   });
 }
 
--- src/js/Currency.js
+++ src/js/Currency.js
@@ -4,20 +4,6 @@ export class Currency{
   }
 
   static calculateConversion(amount, native, target){
-    /*
-    still not sure why this doesn't work/how to get it to work
-    fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${this.native}/eur/${this.amount}`)
-      .then(function(response){
-        console.log(response.url);
-        if(response.status !== 200){
-          //did not receive a 200 response
-        }else if(response[1] === "unsupported-code"){
-          console.log("invalid");
-        } else {
-          console.log(response.conversion_result);
-        }
-      });
-    */
     let promise = new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${native}/${target}/${amount}`;
@@ -26,7 +12,6 @@ export class Currency{
         if(this.status === 200){
           resolve([response, this.amount]);
         } else { 
-          console.log('hey');
           reject([this, response, this.amount]);
         }
       });
