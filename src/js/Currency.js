653179f77c222de2b808a41f0dcc753aec8f1620
PotatoSol <sol.mike.j@gmail.com>
2023-02-10 10:18:26 -0800

    further struggling with api

--- src/js/Currency.js
+++ src/js/Currency.js
@@ -7,9 +7,11 @@ export class Currency{
   }
 
   calculateConversion(){
+    /*
+    still not sure why this doesn't work/how to get it to work
     fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${this.native}/eur/${this.amount}`)
       .then(function(response){
-        console.log(response.url[0]);
+        console.log(response.url);
         if(response.status !== 200){
           //did not receive a 200 response
         }else if(response[1] === "unsupported-code"){
@@ -18,6 +20,25 @@ export class Currency{
           console.log(response.conversion_result);
         }
       });
+    */
+    let native = this.native;
+    let amount = this.amount;
+    let promise = new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${native}/eur/${amount}`;
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if(this.status === 200){
+          resolve([response, this.amount]);
+        } else { 
+          reject([this, response, this.amount]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
+    let a = promise;
+    console.log(a);
   }
 }
 
