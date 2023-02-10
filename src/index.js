70e3467cc5328576f140820e306905cf9caad961
PotatoSol <sol.mike.j@gmail.com>
2023-02-10 09:06:34 -0800

    created html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-//heheh
+//get user amount
+//get user currency
+//get target currency
+//return target currency
+
+//error check - on page
+//  * for invalid currency
+//  * for non-200 api response
+
